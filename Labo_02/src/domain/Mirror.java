db9e232384a1fe0e6cd0c1695f3679b9c42cf7b2
joppe <joppe.duthoit@gmail.com>
2018-10-01 18:29:09 +0200

    Labo_02 deel af

--- Labo_02/src/domain/Mirror.java
+++ Labo_02/src/domain/Mirror.java
@@ -4,11 +4,11 @@ public class Mirror implements Ontcijferaar {
 
     @Override
     public String encypt(String text) {
-        return null;
+        return new StringBuilder(text).reverse().toString();
     }
 
     @Override
     public String decrypt(String text) {
-        return null;
+        return new StringBuilder(text).reverse().toString();
     }
 }
--- Labo_02/src/ui/Encryptor.java
+++ Labo_02/src/ui/Encryptor.java
@@ -1,14 +1,20 @@
-package domain;
+package ui;
+
+import domain.Caesar;
+import domain.Mirror;
+import domain.Ontcijferaar;
 
 public class Encryptor {
     public static void main (String[] args) {
 
         Ontcijferaar caesar = new Caesar(1);
-//        Ontcijferaar mirror = new Mirror();
+        Ontcijferaar mirror = new Mirror();
 
         String encrypt = caesar.encypt("abcdz");
         System.out.println(caesar.encypt("abcdz"));
         System.out.println(caesar.decrypt(encrypt));
 
+        System.out.println(mirror.encypt("test"));
+        System.out.println(mirror.decrypt("tset"));
     }
 }
